269015c32c420475eacc59a5a5bba53a75b30203
feco <hola@fecocode.com>
2020-12-02 20:33:06 -0300

    Add title component

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Titulo</h1>
+</template>
+
+<script>
+export default {
+  name: "TitleComponent"
+};
+</script>
---
+++ src/views/TitleComponentView.vue
@@ -0,0 +1,14 @@
+<template>
+  <title-component />
+</template>
+
+<script>
+import TitleComponent from "../components/TitleComponent";
+
+export default {
+  name: "TitleComponentView",
+  components: {
+    TitleComponent
+  }
+};
+</script>
