bedf8409e6485975a5682f15f7e1f85e5188a681
pilichanampe <pilichanampe@hotmail.com>
2020-12-14 19:12:25 -0300

    Creating InputComponent

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="div">
+    <label for="">Título del input</label>
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/InputComponentView.vue
@@ -0,0 +1,18 @@
+<template>
+  <input-component></input-component>
+</template>
+
+<script>
+import InputComponent from "../components/InputComponent";
+
+export default {
+  name: "InputComponentView",
+  components: {
+    InputComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
