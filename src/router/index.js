fd974b8bc835eafe11fccb8f50cf80506848558b
Pablo <arrosasp@gmail.com>
2020-12-16 21:36:25 -0300

    Finalice desarrollo miniatura post

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Miniatura from "../views/MiniaturaPostView.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path:"/miniatura-post",
+    name: "Miniaturapost",
+    component: Miniatura
   }
 ];
 
---
+++ src/views/MiniaturaPostView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <miniatura-post></miniatura-post>
+  </div> 
+</template>
+
+<script>
+import MiniaturaPost from "../components/MiniaturaPost.vue"
+
+export default {
+  name: 'MiniaturaPostView',
+  components: {
+    MiniaturaPost
+  }
+}
+</script>
